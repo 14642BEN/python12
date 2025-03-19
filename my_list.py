4dd7f790efb54099a7ee4f962e212660d3a4e756
14642BEN <kilonzoben007@gmail.com>
2025-03-19 12:07:19 +0300

    my_list

---
+++ my_list.py
@@ -0,0 +1,16 @@
+my_list =[] # empty list
+my_list.append(10) # add 1 to the list
+my_list.append(20) # add 2 to the list
+my_list.append(30) # add 3 to the list
+my_list.append(40) # add 4 to the list
+print(my_list) # print the list
+my_list.insert(1, 15) # insert 15 at index 1
+print(my_list) # print the list
+my_list.extend([50, 60, 70]) # add 50, 60, 70 to the list
+print(my_list) # print the list
+my_list.pop() # remove the last element
+print(my_list) # print the list
+my_list.sort() # sort the list
+print(my_list) # print the list
+index_30 = my_list.index(30) # get the index of 30
+print(index_30) # print the index
